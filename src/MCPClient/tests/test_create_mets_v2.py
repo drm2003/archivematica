b52dba78e78ad2165b4a86e2f6b3d99f2ac1b909
José Raddaoui Marín <raddaouimarin@gmail.com>
2022-02-18 17:09:06 +0100

    Parametrize test_xml_validation_fail_on_error

--- src/MCPClient/tests/test_create_mets_v2.py
+++ src/MCPClient/tests/test_create_mets_v2.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from contextlib import ExitStack as does_not_raise
 import uuid
 
 from lxml import etree
@@ -306,14 +307,28 @@ def test_aip_mets_normative_directory_structure(
     )
 
 
-def test_xml_validation_fail_on_error(mocker, settings, job, sip_path, sip, file_obj):
+@pytest.mark.parametrize(
+    "fail_on_error, errors, expectation",
+    [
+        (True, ["xml_validation_error"], pytest.raises(Exception)),
+        (True, [], does_not_raise()),
+        (False, ["xml_validation_error"], does_not_raise()),
+        (False, [], does_not_raise()),
+        (None, ["xml_validation_error"], pytest.raises(AttributeError)),
+        (None, [], does_not_raise()),
+    ],
+)
+def test_xml_validation_fail_on_error(
+    mocker, settings, job, sip_path, sip, file_obj, fail_on_error, errors, expectation
+):
     mock_mets = mocker.Mock(**{"serialize.return_value": etree.Element("tag")})
     mocker.patch(
         "create_mets_v2.archivematicaCreateMETSMetadataXML.process_xml_metadata",
-        return_value=(mock_mets, ["xml_validation_error"]),
+        return_value=(mock_mets, errors),
     )
-    settings.XML_VALIDATION_FAIL_ON_ERROR = True
-    with pytest.raises(Exception) as error:
+    if fail_on_error is not None:
+        settings.XML_VALIDATION_FAIL_ON_ERROR = fail_on_error
+    with expectation:
         main(
             job,
             sipType="SIP",
@@ -323,8 +338,8 @@ def test_xml_validation_fail_on_error(mocker, settings, job, sip_path, sip, file
             includeAmdSec=False,
             createNormativeStructmap=False,
         )
-    assert "Error generating AIP METS" in str(error)
-    assert (
-        "Error(s) processing and/or validating XML metadata:\n\t- xml_validation_error"
-        in job.get_stderr()
-    )
+    if errors:
+        assert (
+            "Error(s) processing and/or validating XML metadata:\n\t- xml_validation_error"
+            in job.get_stderr()
+        )
